a9c6ca7f5b4993bb14c30944a3a3c3933a9fdaee
malloc <1554243238@qq.com>
2025-03-28 16:36:55 +0800

    delete code about stopping

--- scripts/docker_into.sh
+++ scripts/docker_into.sh
@@ -3,6 +3,6 @@
 # xhost +local:root 1>/dev/null 2>&1
 docker exec \
     -u root \
-    -it malloc_reproduce \
+    -it malloc_java \
     /bin/zsh
 #
--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,6 +1,6 @@
-echo "stop and rm docker" 
-docker stop malloc_reproduce > /dev/null
-docker rm -v -f malloc_reproduce > /dev/null
+# echo "stop and rm docker" 
+# docker stop malloc_reproduce > /dev/null
+# docker rm -v -f malloc_reproduce > /dev/null
 
 
 MONITOR_HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
@@ -20,7 +20,7 @@ gid="$(id -g)"
 
 docker run -it -d \
 --gpus all \
---name malloc_reproduce \
+--name malloc_java \
 --privileged=true \
 --restart=always \
 -e DISPLAY=$display \
@@ -31,5 +31,7 @@ docker run -it -d \
 -e DOCKER_GRP_ID="${gid}" \
 -e NVIDIA_DRIVER_CAPABILITIES=compute,utility \
 -e NVIDIA_VISIBLE_DEVICES=all \
--p 49763:22 \
+-p 49764:22 \
+-p 66666:6666 \
+-v /home/malloc/Documents/project/mystation:/share/mystation
 linux22:gpuReproduce
